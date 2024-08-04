daa8014e7e6c3154ad4b6239c6e76e79905a402e
rivaldoe <redah@umd.edu>
2024-08-04 02:51:38 -0400

    updated from popos

--- wezterm.lua
+++ wezterm.lua
@@ -1,18 +1,12 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
+local act = wezterm.action
 
--- This table will hold the configuration.
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
 config.audible_bell = "Disabled"
 config.keys = {
 	{
@@ -20,9 +14,23 @@ config.keys = {
 		mods = "CMD",
 		action = wezterm.action.ToggleFullScreen,
 	},
+	{
+		key = "E",
+		mods = "CTRL|SHIFT",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
 }
 -- config.color_scheme = "Catppuccin Macchiato"
 config.color_scheme = "Colors (base16)"
+-- config.color_scheme = "Grayscale Dark (base16)"
+-- config.color_scheme = "Grayscale (dark) (terminal.sexy)"
 config.adjust_window_size_when_changing_font_size = false
 -- Set background to same color as neovim
 config.colors = {
@@ -30,7 +38,7 @@ config.colors = {
 
 	foreground = "silver", -- [silver] The default text color
 	background = "black", -- [black]  The default background color
-	cursor_bg = "#52ad70", -- [#52ad70] Overrides the cell background color when the current cell is occupied by the cursor and the cursor style is set to Block
+	-- cursor_bg = "#52ad70", -- [#52ad70] Overrides the cell background color when the current cell is occupied by the cursor and the cursor style is set to Block
 	cursor_fg = "black", -- [black]   Overrides the text color when the current cell is occupied by the cursor
 	cursor_border = "#52ad70", -- [#52ad70] Specifies the border color of the cursor when the cursor style is set to Block, of the color of the vertical or horizontal bar when the cursor style is set to Bar or Underline.
 	selection_fg = "black", -- [black]   The foreground color of selected text
@@ -42,9 +50,10 @@ config.colors = {
 	--   indexed     	= {[136]="#af8700"} , -- Arbitrary colors of the palette in the range from 16 to 255
 }
 
-config.window_background_opacity = 0.98 -- [1.0] alpha channel value with floating point numbers in the range 0.0 (meaning completely translucent/transparent) through to 1.0 (meaning completely opaque)
-config.colors.background = "#111111"
-config.font_size = 13
+-- config.window_background_opacity = 0.69 + 0.0420 + 0.19 -- [1.0] alpha channel value with floating point numbers in the range 0.0 (meaning completely translucent/transparent) through to 1.0 (meaning completely opaque)
+-- config.colors.background = "#111111"
+config.font_size = 15
+config.cursor_thickness = "4px"
 config.font = wezterm.font_with_fallback({
 	"JetBrains Mono",
 })
@@ -61,7 +70,6 @@ config.window_padding = {
 	top = 0,
 	bottom = 0,
 }
-
 config.tab_bar_at_bottom = true
 config.freetype_load_target = "HorizontalLcd"
 
